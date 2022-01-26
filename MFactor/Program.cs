5a64c95a510c4344da29a2d6dc80de32bf02eb8d
Agnieszka Gregulec <agnieszka.gregulec@gmail.com>
2022-01-26 19:56:13 +0100

    upload

---
+++ MFactor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ManufacturerPizza manufacturer = new ManufacturerPizza();
+            Pizza[] tab = new Pizza[3];
+
+            tab[0] = manufacturer.producePizza("Margherita");
+            Console.WriteLine("--------------------------------------------------------------");
+            tab[1] = manufacturer.producePizza("Capricciosa");
+            Console.WriteLine("--------------------------------------------------------------");
+            tab[2] = manufacturer.producePizza("Pepperoni");
+            Console.ReadLine();
+        }
+    }
+}
